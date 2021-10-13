ef68e4328d62844c5200883dd724bc62b5e9c4a8
marcelofi <marce.fernandezi@gmail.com>
2021-10-13 14:59:38 -0400

    Fixed the automation secibd test case

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -12,13 +12,13 @@ describe("Search elements",()=>{
 
         })
     })
-    it('search from elementes with no results',()=>{ 
+    it('search for elementes with no results',()=>{ 
         cy.fixture('index').then((index)=>{
             cy.get(index.searchBox).type('qwerty');
             cy.get(index.searchButton).click();
         })
         cy.fixture('searchresults').then((searchresult)=>{ 
-            cy.get(searchresult.alert).should('contain','No results were for you search')
+            cy.get(searchresult.heading).should('contain','0 results have been found')
 
         })
     })
