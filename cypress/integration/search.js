9c4158e0faa620bed5a40ca564fd6249d471ad95
marcelofi <marce.fernandezi@gmail.com>
2021-10-13 15:18:27 -0400

    Adding commands to the repetitive steps

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -3,20 +3,14 @@ describe("Search elements",()=>{
         cy.visit('/')
     })
     it("Search for elements with multiple results",()=>{ 
-        cy.fixture('index').then((index)=>{ 
-            cy.get(index.searchBox).type('dress')
-            cy.get(index.searchButton).click();
-        })
+        cy.search('dress')
         cy.fixture('searchresults').then((searchresult)=>{ 
             cy.get(searchresult.title).should('contain','dress')
 
         })
     })
     it('search for elementes with no results',()=>{ 
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('qwerty');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('qwerty')
         cy.fixture('searchresults').then((searchresult)=>{ 
             cy.get(searchresult.heading).should('contain','0 results have been found')
 
